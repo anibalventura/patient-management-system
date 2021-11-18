5f6cb35269dc1938b1f1282f0573802f74d7b194
Anibal Ventura <anibalventura20@gmail.com>
2021-11-18 18:02:27 -0400

    Add FileHandler layer.

---
+++ FileHandler/FileSaver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandler
+{
+    public class FileSaver
+    {
+        public string SaveFile(string fileName, string directory)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    string[] fileNameSplit = fileName.Split(@"\");
+                    string filename = fileNameSplit[(fileNameSplit.Length - 1)];
+
+                    CreateDirectory(directory);
+
+                    string destination = directory + filename;
+
+                    File.Copy(fileName, destination, true);
+
+                    return destination;
+                }
+
+                return null;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private void CreateDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
